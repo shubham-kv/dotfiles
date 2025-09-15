b681093ad61191618ee04189480815a67a373f84
Shubham Vishwakarma <90132246+shubham-kv@users.noreply.github.com>
2025-09-15 21:32:23 +0530

    config(nvim): Format init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -34,7 +34,7 @@ vim.o.ignorecase = true
 vim.o.hlsearch = false
 
 vim.o.cursorline = true
-vim.o.signcolumn = 'yes'
+vim.o.signcolumn = "yes"
 
 vim.o.scrolloff = 2
 vim.o.splitright = true
@@ -68,11 +68,11 @@ local function bootstrap_pckr()
 
   if not (vim.uv or vim.loop).fs_stat(pckr_path) then
     vim.fn.system({
-      'git',
-      'clone',
+      "git",
+      "clone",
       "--filter=blob:none",
-      'https://github.com/lewis6991/pckr.nvim',
-      pckr_path
+      "https://github.com/lewis6991/pckr.nvim",
+      pckr_path,
     })
   end
 
@@ -81,63 +81,52 @@ end
 
 bootstrap_pckr()
 
-safe_require('pckr', function(pckr)
+safe_require("pckr", function(pckr)
   pckr.add({
-    'nvim-lua/plenary.nvim';
-    'nvim-tree/nvim-web-devicons';
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
 
     -- Colorschemes
-    'folke/tokyonight.nvim';
+    "folke/tokyonight.nvim",
 
     -- File explorer & navigation
-    { 'nvim-tree/nvim-tree.lua',
-      requires = { 'nvim-tree/nvim-web-devicons' },
+    {
+      "nvim-tree/nvim-tree.lua",
+      requires = { "nvim-tree/nvim-web-devicons" },
       config = function()
-        safe_require('nvim-tree', {
-          sort = {
-            sorter = 'case_sensitive',
-          },
-          view = {
-            width = 40,
-            adaptive_size = true
-          },
-          update_focused_file = {
-            enable = true,
-            update_root = false,
-          },
-          renderer = {
-            group_empty = true,
-          },
-          filters = {
-            dotfiles = false,
-            git_ignored = false
-          },
+        safe_require("nvim-tree", {
+          renderer = { group_empty = true },
+          sort = { sorter = "case_sensitive" },
+          view = { width = 40, adaptive_size = true },
+          filters = { dotfiles = false, git_ignored = false },
+          update_focused_file = { enable = true, update_root = false },
         })
-      end
-    };
+      end,
+    },
 
-    { 'nvim-mini/mini.nvim',
+    {
+      "nvim-mini/mini.nvim",
       config = function()
         -- Quick navigation
-        safe_require('mini.pick')
+        safe_require("mini.pick")
 
         -- Code completion
-        safe_require('mini.snippets')
-        safe_require('mini.completion')
-        safe_require('mini.pairs')
+        safe_require("mini.snippets")
+        safe_require("mini.completion")
+        safe_require("mini.pairs")
 
         -- Git
-        safe_require('mini.git')
-        safe_require('mini.diff', {
+        safe_require("mini.git")
+        safe_require("mini.diff", {
           view = {
-            style = 'sign',
-            signs = { add = '+', change = '~', delete = '-' },
-          }
+            style = "sign",
+            signs = { add = "+", change = "~", delete = "-" },
+          },
         })
 
         -- Appearance
-        safe_require('mini.statusline')
-        safe_require('mini.starter', function(starter)
+        safe_require("mini.statusline")
+        safe_require("mini.starter", function(starter)
           starter.setup({
             evaluate_single = true,
             items = {
@@ -146,48 +135,52 @@ safe_require('pckr', function(pckr)
             },
             content_hooks = {
               starter.gen_hook.adding_bullet(),
-              starter.gen_hook.aligning('center', 'center'),
+              starter.gen_hook.aligning("center", "center"),
               starter.gen_hook.padding(3, 0),
             },
           })
         end)
-      end
-    };
+      end,
+    },
 
     -- Syntax highlighting
-    { 'nvim-treesitter/nvim-treesitter',
-      run = ':TSUpdate',
+    {
+      "nvim-treesitter/nvim-treesitter",
+      run = ":TSUpdate",
       config = function()
-        safe_require('nvim-treesitter.configs', {
+        safe_require("nvim-treesitter.configs", {
           auto_install = true,
           highlight = { enable = true },
           indent = { enable = true },
         })
-      end
-    };
+      end,
+    },
 
     -- LSPs, Formatting, etc.
-    'neovim/nvim-lspconfig';
+    "neovim/nvim-lspconfig",
 
-    { 'mason-org/mason.nvim',
+    {
+      "mason-org/mason.nvim",
       config = function()
-        safe_require('mason')
-      end
-    };
+        safe_require("mason")
+      end,
+    },
 
     -- Diagnostics, Code actions, etc.
-    { 'nvimtools/none-ls.nvim',
+    {
+      "nvimtools/none-ls.nvim",
       requires = { "nvim-lua/plenary.nvim" },
       config = function()
-        safe_require('null-ls', function(null_ls)
+        safe_require("null-ls", function(null_ls)
           null_ls.setup({
             sources = {
-              null_ls.builtins.formatting.prettierd
+              null_ls.builtins.formatting.prettierd,
+              null_ls.builtins.formatting.stylua,
             },
           })
         end)
-      end
-    }
+      end,
+    },
   })
 end)
 
@@ -195,86 +188,86 @@ end)
 
 -- Theme ------------------------ {{
 
-local theme = 'tokyonight'
+local theme = "tokyonight"
 local ok = pcall(vim.cmd.colorscheme, theme)
 
 if not ok then
-  print('Failed to set \'' .. theme .. '\' colorscheme')
+  print("Failed to set '" .. theme .. "' colorscheme")
 end
 
 -- }}
 
 -- Keymaps ------------------------ {{
 
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
-vim.keymap.set('n', '<leader>s', '<cmd>source %<cr>', {desc = 'Read Vim or Ex commands from current file'})
-vim.keymap.set('n', '<leader>w', '<cmd>write<cr>', {desc = 'Save file'})
-vim.keymap.set('n', '<leader>q', '<cmd>quit<cr>', {desc = 'Close current window'})
+vim.keymap.set("n", "<leader>s", "<cmd>source %<cr>", { desc = "Read Vim or Ex commands from current file" })
+vim.keymap.set("n", "<leader>w", "<cmd>write<cr>", { desc = "Save file" })
+vim.keymap.set("n", "<leader>q", "<cmd>quit<cr>", { desc = "Close current window" })
 
-vim.keymap.set('n', '<leader>tn', '<cmd>tabnew<cr>', {desc = 'Open new tab'})
-vim.keymap.set('n', '<leader>th', '<cmd>tabprevious<cr>', {desc = 'Open previous tab to the left'})
-vim.keymap.set('n', '<leader>tl', '<cmd>tabnext<cr>', {desc = 'Open next tab to the right'})
-vim.keymap.set('n', '<leader>tx', '<cmd>tabclose<cr>', {desc = 'Close current tab'})
+vim.keymap.set("n", "<leader>tn", "<cmd>tabnew<cr>", { desc = "Open new tab" })
+vim.keymap.set("n", "<leader>th", "<cmd>tabprevious<cr>", { desc = "Open previous tab to the left" })
+vim.keymap.set("n", "<leader>tl", "<cmd>tabnext<cr>", { desc = "Open next tab to the right" })
+vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<cr>", { desc = "Close current tab" })
 
 vim.keymap.set("n", "<leader>d", function()
   vim.diagnostic.open_float(nil, { focus = true })
 end, { desc = "Show diagnostics under cursor" })
 
-vim.keymap.set('n', '<leader>p', vim.cmd.NvimTreeToggle)
+vim.keymap.set("n", "<leader>p", vim.cmd.NvimTreeToggle)
 
 -- mini.pick keymaps
-vim.keymap.set('n', '<leader>fb', '<cmd>Pick buffers<cr>', {desc = 'Search open buffers'})
-vim.keymap.set('n', '<leader>ff', '<cmd>Pick files<cr>', {desc = 'Search all files'})
-vim.keymap.set('n', '<leader>fh', '<cmd>Pick help<cr>', {desc = 'Search through help'})
-vim.keymap.set('n', '<leader>fg', '<cmd>Pick grep_live<cr>', {desc = 'Grep through files'})
+vim.keymap.set("n", "<leader>fb", "<cmd>Pick buffers<cr>", { desc = "Search open buffers" })
+vim.keymap.set("n", "<leader>ff", "<cmd>Pick files<cr>", { desc = "Search all files" })
+vim.keymap.set("n", "<leader>fh", "<cmd>Pick help<cr>", { desc = "Search through help" })
+vim.keymap.set("n", "<leader>fg", "<cmd>Pick grep_live<cr>", { desc = "Grep through files" })
 
 -- }}
 
 -- LSP Setup ---------------------- {{
 
-vim.api.nvim_create_autocmd('LspAttach', {
-  desc = 'LSP actions',
+vim.api.nvim_create_autocmd("LspAttach", {
+  desc = "LSP actions",
   callback = function(event)
-    local opts = {buffer = event.buf}
+    local opts = { buffer = event.buf }
 
     -- Display documentation of the symbol under the cursor
-    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+    vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
 
     -- Jump to the definition
-    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+    vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
 
     -- Format current file
-    vim.keymap.set({'n', 'x'}, 'gq', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+    vim.keymap.set({ "n", "x" }, "gq", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
 
     -- Displays a function's signature information
-    vim.keymap.set('i', '<C-s>', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+    vim.keymap.set("i", "<C-s>", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
 
     -- Jump to declaration
-    vim.keymap.set('n', '<leader>ld', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+    vim.keymap.set("n", "<leader>ld", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
 
     -- Lists all the implementations for the symbol under the cursor
-    vim.keymap.set('n', '<leader>li', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+    vim.keymap.set("n", "<leader>li", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
 
     -- Jumps to the definition of the type symbol
-    vim.keymap.set('n', '<leader>lt', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+    vim.keymap.set("n", "<leader>lt", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
 
     -- Lists all the references
-    vim.keymap.set('n', '<leader>lr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+    vim.keymap.set("n", "<leader>lr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
 
     -- Renames all references to the symbol under the cursor
-    vim.keymap.set('n', '<leader>ln', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+    vim.keymap.set("n", "<leader>ln", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
 
     -- Selects a code action available at the current cursor position
-    vim.keymap.set('n', '<leader>la', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+    vim.keymap.set("n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
   end,
 })
 
 -- Use the "legacy setup" on older Neovim version.
 -- The new api is only available on Neovim v0.11 or greater.
 local function lsp_setup(server, opts)
-  if vim.fn.has('nvim-0.11') == 0 then
-    safe_require('lspconfig', function(lspconfig)
+  if vim.fn.has("nvim-0.11") == 0 then
+    safe_require("lspconfig", function(lspconfig)
       lspconfig[server].setup(opts)
     end)
     return
@@ -287,13 +280,13 @@ local function lsp_setup(server, opts)
   vim.lsp.enable(server)
 end
 
-lsp_setup('vimls', {})
-lsp_setup('lua_ls', {
-  settings = { Lua = { diagnostics = { globals = {"vim"} } } }
+lsp_setup("vimls", {})
+lsp_setup("lua_ls", {
+  settings = { Lua = { diagnostics = { globals = { "vim" } } } },
 })
-lsp_setup('ts_ls', {})
-lsp_setup('clangd', {})
-lsp_setup('marksman', {})
+lsp_setup("ts_ls", {})
+lsp_setup("clangd", {})
+lsp_setup("marksman", {})
 -- lsp_setup('rust_analyzer', {})
 
 -- }}
@@ -303,8 +296,8 @@ lsp_setup('marksman', {})
 -- Diagnostics
 vim.diagnostic.config({
   virtual_text = {
-    prefix = '',    -- Customize prefix (●, →, » etc.)
-    spacing = 2,    -- Space between text and virtual text
+    prefix = "",  -- Customize prefix (●, →, » etc.)
+    spacing = 2,  -- Space between text and virtual text
     severity = nil, -- Show all severities (can set to vim.diagnostic.severity.ERROR if needed)
     format = function(diagnostic)
       local icons = {
@@ -318,22 +311,21 @@ vim.diagnostic.config({
   },
   signs = {
     text = {
-        [vim.diagnostic.severity.ERROR] = "✘",
-        [vim.diagnostic.severity.WARN] = "▲",
-        [vim.diagnostic.severity.INFO] = "»",
-        [vim.diagnostic.severity.HINT] = "⚑",
+      [vim.diagnostic.severity.ERROR] = "✘",
+      [vim.diagnostic.severity.WARN] = "▲",
+      [vim.diagnostic.severity.INFO] = "»",
+      [vim.diagnostic.severity.HINT] = "⚑",
     },
     linehl = {
-      [vim.diagnostic.severity.ERROR] = 'ErrorMsg',
+      [vim.diagnostic.severity.ERROR] = "ErrorMsg",
     },
     numhl = {
-      [vim.diagnostic.severity.WARN] = 'WarningMsg',
+      [vim.diagnostic.severity.WARN] = "WarningMsg",
     },
-  },             -- Show signs in the gutter
-  underline = true,         -- Underline errors/warnings
+  },                       -- Show signs in the gutter
+  underline = true,        -- Underline errors/warnings
   update_in_insert = false, -- Don't update while typing (prevents flicker)
-  severity_sort = true,     -- Sort by severity
+  severity_sort = true,    -- Sort by severity
 })
 
 -- }}
-
